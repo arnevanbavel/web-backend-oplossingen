9f9eb1295706ca0f1a0e7a41b4a71608559f313b
arnevanbavel <arne.vanbavel@student.kdg.be>
2014-09-23 11:55:14 +0200

    oefening 1 comments gemaakt

---
+++ opdracht-comments.php
@@ -0,0 +1,33 @@
+<?php
+$voornaam = "Arne";
+$achternaam = "Van Bavel"
+
+
+
+/*
+Arne Van Bavel
+email: arne.vanbavel@student.kdg.be
+*/	
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Voorbeeld var_dump()</title>
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/global.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/directory.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/facade.css">
+</head>
+<body class="web-backend-inleiding">
+
+	<section class="body">
+
+		<h1><?php echo $voornaam; ?>  <?php echo $achternaam; ?></h1>
+
+	</section>
+
+</body>
+</html>
